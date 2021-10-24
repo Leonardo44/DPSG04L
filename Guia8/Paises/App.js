9294252616efc794bf40745316750289a797c955
leonarrdo404 <leo@ese.plus>
2021-10-24 14:24:19 -0600

    guia8 completed

--- Guia8/Paises/App.js
+++ Guia8/Paises/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -16,6 +16,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Alert
 } from 'react-native';
 
 import {
@@ -25,87 +26,71 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Formulario from './src/Components/Formulario';
+import Pais from './src/Components/pais';
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [busqueda, guardarbusqueda] = useState({
+    pais: ''
+  })
+  const [consultar, guardarconsultar] = useState(false);
+  const [resultado, guardarresultado] = useState({});
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  useEffect(() => {
+    const { pais } = busqueda;
+    const consultarPais = async () => {
+      if (consultar) {
+        const url = `https://servicodados.ibge.gov.br/api/v1/paises/${pais}`;
+        try {
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+          guardarresultado(resultado);
+          guardarconsultar(false);
+        } catch (error) {
+          mostrarAlerta();
+        }
+      }
+    };
+    consultarPais();
+  },[consultar]);
+    
+  const mostrarAlerta = () => {
+    Alert.alert('Error', 'No hay resultado intenta con otra ciudad o país'), [{ Text: 'Ok' }];
+  };
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={styles.app}>
+      <ScrollView>
+        <View style={styles.contenido}>
+          <Formulario
+            busqueda={busqueda}
+            guardarbusqueda={guardarbusqueda}
+            guardarconsultar={guardarconsultar}
+          />
+        </View>
+        <View style={styles.contenido}>
+          <Pais
+            busqueda={busqueda}
+            resultado={resultado}></Pais>
         </View>
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  app: {
+    flex: 1,
+    backgroundColor: 'rgb(71,149,212)',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  contenido: {
+    margin: '2.5%',
   },
 });
 
